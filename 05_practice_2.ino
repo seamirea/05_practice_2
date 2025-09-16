9369af7a9e5a23f9d59a813e9f941a669a54e5fa
seamirea <85031781+seamirea@users.noreply.github.com>
2025-09-16 17:00:39 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,23 @@
+int led = 7;  // led is connected to pin 7 
+
+void setup() {
+  pinMode(led, OUTPUT);  // assign pin as an output 
+}
+
+void loop() {
+  // the led will be on for 1 second 
+  digitalWrite(led, HIGH);
+  delay(1000);
+
+  // led will flash 5 times 
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(led, LOW);   
+    delay(200);               
+    digitalWrite(led, HIGH);  
+    delay(200);               
+  }
+
+  digitalWrite(led, LOW);
+  while (1) {
+  }
+}
